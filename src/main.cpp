6406750ccc0e8bc3faa55730aedb48469d8beca8
kai-1208 <ra23070n@st.omu.ac.jp>
2026-05-15 16:33:13 +0900

    userbuttonのみで操作可能にした

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,60 @@
 #include "mbed.h"
 #include "FP.hpp"
 #include "key.hpp"
-#include "serial_read.hpp"
+// #include "serial_read.hpp"
 
 BufferedSerial pc(USBTX, USBRX, 115200);
 
-serial_unit serial(pc);
+// serial_unit serial(pc);
 
 CAN can1(PA_11, PA_12, (int)1e6);
 
 DigitalIn limit_sw1(PC_10);
-// DigitalIn bt1(BUTTO1N1);
+DigitalIn bt1(BUTTON1);
 
 FP fp(35, can1);
 
+bool is_button_pressed = false;
+
+// void button_read() {
+//     while (true) {
+//         if (Triangle) {
+//             if (limit_sw1.read() == 1) {
+//                 fp.pwm[1] = -6500;
+//             } else {
+//                 fp.pwm[1] = 0;
+//             }
+//         } else if (Cross) {
+//             fp.pwm[1] = 2000;
+//         } else {
+//             fp.pwm[1] = 0;
+//         }
+//         printf("pwm[1]: %d\n", fp.pwm[1]);
+//         ThisThread::sleep_for(10ms);
+//     }
+// }
+
+// user button で操作
 void button_read() {
     while (true) {
-        if (Triangle) {
+        if (bt1.read() == 0) {
+            is_button_pressed = true;
             if (limit_sw1.read() == 1) {
-                fp.pwm[1] = -13000;
+                fp.pwm[1] = -6750;
             } else {
                 fp.pwm[1] = 0;
             }
-        } else if (Cross) {
-            fp.pwm[1] = 2000;
         } else {
-            fp.pwm[1] = 0;
+            if (is_button_pressed) {
+                fp.pwm[1] = 2000;
+            } else {
+                fp.pwm[1] = 0;
+            }
         }
         printf("pwm[1]: %d\n", fp.pwm[1]);
         ThisThread::sleep_for(10ms);
     }
 }
-
-// user button で操作
-// void button_read() {
-//     while (true) {
-//         if (bt1.read() == 0) {
-//             if (limit_sw1.read() == 1) {
-//                 fp.pwm[1] = 5000;
-//             } else {
-//                 fp.pwm[1] = 0;
-//             }
-//         } else {
-//             if (limit_sw2.read() == 1) {
-//                 fp.pwm[1] = -1000;
-//             } else {
-//                 fp.pwm[1] = 0;
-//             }
-//         }
-//     }
-// }
 
 void can_send() {
     while (true) {
@@ -60,9 +65,9 @@ void can_send() {
 
 int main () {
     limit_sw1.mode(PullUp);
-    // bt1.mode(PullUp);
-    Thread serial_thread;
-    serial_thread.start(serial_read);
+    bt1.mode(PullUp);
+    // Thread serial_thread;
+    // serial_thread.start(serial_read);
     Thread button_thread;
     button_thread.start(button_read);
     Thread can_thread;
